fff1a00614bb0c50d77a0f05e7f0046353677f80
Jia T <66760300+jia-von@users.noreply.github.com>
2022-09-09 18:45:23 -0600

    Add files via upload

---
+++ scrape_organisation.py
@@ -0,0 +1,28 @@
+import requests
+import json
+import pandas as pd
+
+# 1. Use pandas to return dataframe objects with columns, id, login, and url
+def scrape_org(api_url):
+    url = requests.get(api_url).text
+    read_json = pd.read_json(url)
+    return read_json['url']
+
+
+# 2. This function is used to get the next link provided by GitHub Header
+def link_head(api_url):
+    dict_head = requests.head(api_url).links['next']
+    return dict_head['url']
+
+# 3. A function to get description, name, company, blog, location, email
+def get_org_info(git_org_url):
+    url = requests.get(git_org_url).text
+    read_json = json.loads(url)
+    final_output = pd.Series(read_json,index=['name','company','blog','location','description'])
+    return final_output
+    
+# 4. Insert url to get details
+
+
+
+
---
+++ scrape_url.py
@@ -0,0 +1,25 @@
+from scrape_organisation import scrape_org
+from sqlalchemy import create_engine
+from sql_alchemy import db_connection
+
+# create sqlalchemy engine
+# Scrape URL and update the database
+# read connection parameters
+engine = create_engine(db_connection)
+
+# Create table code block
+
+# connect to the PostgreSQL server
+with engine.connect() as conn:
+    if engine.has_table('github_url'):
+        result_org = scrape_org('https://api.github.com/organizations?per_page=100')
+        result_org.to_sql('github_url',schema='public', con=conn, if_exists='append')
+    else:
+        engine.execute('''
+        CREATE TABLE github_url (
+            index int,
+            url varchar
+        )
+        ''')
+        result_org = scrape_org('https://api.github.com/organizations?per_page=100')
+        result_org.to_sql('github_url',schema='public', con=conn, if_exists='append')
