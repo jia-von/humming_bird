aefc0d6945fbf036430be921d7c7bee7ffabb258
jia-von <jia-von6760300+jia-von@users.noreply.github.com>
2022-09-12 19:13:23 -0600

    added references

--- check_github_profile.py
+++ check_github_profile.py
@@ -7,6 +7,7 @@ from sql_alchemy import db_connection
 engine = create_engine(db_connection)
 
 # Testing with reading sql from database using sqlalchemy
+# Refer: https://towardsdatascience.com/sqlalchemy-python-tutorial-79a577141a91
 metadata_obj = MetaData()
 meta_table = Table('github_url',metadata_obj,autoload=True,autoload_with=engine)
 print(meta_table.columns.keys())
