db7bbc6aee9654c2f262af87ee63df5823b63629
jia-von <jia-von6760300+jia-von@users.noreply.github.com>
2022-09-13 20:14:52 -0600

    completed iteration for object

--- html_scrapper.py
+++ html_scrapper.py
@@ -1,17 +1,19 @@
-from bs4 import BeautifulSoup, SoupStrainer
+from bs4 import BeautifulSoup, SoupStrainer, PageElement, Tag
 import soupsieve as sv
 from requests import request
 
 # 1. Get HTML content response using request()
 test_url = 'https://github.com/airbnb'
 test_response = request(method='Get',url=test_url).text
+s =''
 
-# Make a soup strainer https://www.crummy.com/software/BeautifulSoup/bs4/doc/#soupstrainer
+# 2. Make a soup strainer https://www.crummy.com/software/BeautifulSoup/bs4/doc/#soupstrainer
 # combine dictionary with multiple items https://www.w3schools.com/python/python_dictionaries.asp
 strainer_url = SoupStrainer(name=['a','span'],attrs={'itemprop': ['url', 'email','location']})
 
-# Instantiate object refer: https://www.crummy.com/software/BeautifulSoup/bs4/doc/#quick-start
+# 3. Instantiate object refer: https://www.crummy.com/software/BeautifulSoup/bs4/doc/#quick-start
 soup_obj = BeautifulSoup(test_response,'html.parser',parse_only=strainer_url)
 
-# make a second parser with item_prop
-print(soup_obj)
+# Create function to obtain multiple items, need too use iteration because I can't do mapping yet
+for x in soup_obj.contents:
+    print(x.text)
